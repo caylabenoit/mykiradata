4abd14cfba6f928fec6ef19b1544a762e5bf9ce0
Benoit CAYLA (benoit@famillecayla.fr) <benoit@famillecayla.fr>
2017-01-20 15:48:53 +0100

    little changes

--- web/theme/js/dgmcharts.js
+++ web/theme/js/dgmcharts.js
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 /**
  * dynamically Build a list with score (label + score with progress bar)
  * The dataset must return 3 fields in this order :
